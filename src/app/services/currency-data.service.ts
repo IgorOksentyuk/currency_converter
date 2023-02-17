8f8673838f12c74732b21748ac2213fdd5cf530f
Igor Oksentyuk <winbka.lem@gmail.com>
2023-02-17 17:01:07 +0200

    Added observable for data method

--- src/app/services/currency-data.service.ts
+++ src/app/services/currency-data.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CurrencyResp } from '../models/data.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyDataService {
-  currentJSON: any = [];
-
   constructor(public http: HttpClient) {}
 
-  getCurrencyData(currency: string) {
-    let url = `https://api.exchangerate.host/latest?base=${currency}`;
-    return this.http.get(url);
+  getCurrencyData(currency: string): Observable<CurrencyResp> {
+    let urlBase = 'https://api.exchangerate.host';
+    let url = urlBase + `/latest?base=${currency}`;
+
+    return this.http.get(url) as Observable<CurrencyResp>;
   }
 }
