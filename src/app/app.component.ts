2db8fc2ba91611b0508f1de4ed9af03665ee43da
Igor Oksentyuk <winbka.lem@gmail.com>
2023-02-21 12:32:21 +0200

    Added possibility to enter count of chosen currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { CurrencyDataService } from './services/currency-data.service';
+import { CurrencyForm } from './models/currency-form.model';
 
 @Component({
   selector: 'app-root',
@@ -11,34 +12,51 @@ import { CurrencyDataService } from './services/currency-data.service';
 export class AppComponent {
   firstCurrency = 'USD';
   secondCurrency = 'USD';
-  result = '1.00';
+  result = 1;
   currencies: string[] = [];
   subscription: Subscription;
+  currencyValue: number;
 
   constructor(
     public currencyDataSvc: CurrencyDataService,
     private cdref: ChangeDetectorRef
   ) {}
 
+  form: CurrencyForm = {
+    count: 1,
+  };
+
   ngOnInit(): void {
     this.currencies = ['USD', 'UAH', 'EUR'];
     this.cdref.detectChanges();
+    this.getCurrencyValue();
   }
 
   setBaseCurrency(currency: string) {
     this.firstCurrency = currency;
+    this.getCurrencyValue();
   }
 
   setSecondCurrency(currency: string) {
     this.secondCurrency = currency;
+    this.getCurrencyValue();
   }
 
-  convert() {
+  getCurrencyValue() {
     this.subscription = this.currencyDataSvc
       .getCurrencyData(this.firstCurrency)
       .subscribe((data) => {
-        this.result = data.rates[this.secondCurrency].toFixed(2);
+        this.currencyValue = data.rates[this.secondCurrency];
       });
+  }
+
+  getnputValue(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.form.count = Number(target.value);
+  }
+
+  showResult() {
+    this.result = this.form.count * this.currencyValue;
   }
 
   ngOnDestroy(): void {
